79f76a7bb810f0e1af9dfea98f6338a4a7638219
Francisco Marcano <fjmn2001@gmail.com>
2024-02-18 03:04:03 -0400

    adding menuItems v2

--- src/sections/shared/ui/sidebar.tsx
+++ src/sections/shared/ui/sidebar.tsx
@@ -1,27 +1,6 @@
 import { Atom, Calculator, LucideLayoutDashboard } from "lucide-react";
-import { ReactNode } from "react";
 
-interface SidebarMenuItemProps {
-	path: string;
-	icon: ReactNode;
-	title: string;
-	subtitle: string;
-}
-
-export const SidebarMenuItem = ({ path, icon, title, subtitle }: SidebarMenuItemProps) => {
-	return (
-		<a
-			href={path}
-			className="w-full px-2 inline-flex space-x-2 items-center border-b border-slate-700 py-3 bg-blue-800 hover:bg-white/5 transition ease-linear duration-150"
-		>
-			<div>{icon}</div>
-			<div className="flex flex-col">
-				<span className="text-lg font-bold leading-5 text-white">{title}</span>
-				<span className="text-sm text-white/50 hidden md:block">{subtitle}</span>
-			</div>
-		</a>
-	);
-};
+import { SidebarMenuItem } from "@/sections/shared/ui/sidebar-menu-item";
 
 const menuItems = [
 	{
