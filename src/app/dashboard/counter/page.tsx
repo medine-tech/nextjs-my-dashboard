c9e7747be3e2bc2e2bfb888682adf48a16310ea5
Francisco Marcano <fjmn2001@gmail.com>
2024-02-18 03:33:31 -0400

    adding cart counter component

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from "next";
+
+import { CartCounter } from "@/sections/shopping-cart/cart-counter";
+
+export const metadata: Metadata = {
+	title: "Shopping Cart",
+	description: "Shopping Cart",
+};
+
 export default function CounterPage() {
 	return (
-		<div>
-			<h1>Counter Page</h1>
+		<div className={"flex flex-col items-center justify-center w-full h-full"}>
+			<h1>Products in basket</h1>
+			<CartCounter value={20} />
 		</div>
 	);
 }
