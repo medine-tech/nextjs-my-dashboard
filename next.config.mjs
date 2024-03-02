dc6c881ab0184b1539643377603308176bb2cbe0
Francisco Marcano <fjmn2001@gmail.com>
2024-03-02 19:32:13 -0400

    adding pokemon page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
 };
 
 export default nextConfig;
--- src/modules/pokemons/domain/pokemon.ts
+++ src/modules/pokemons/domain/pokemon.ts
@@ -1,4 +1,10 @@
+import { Mfe, Sprites, Type } from "@/modules/pokemons/infrastructure/response";
+
 export default interface Pokemon {
 	id: string;
 	name: string;
+	sprites: Sprites;
+	moves: Mfe[];
+	types: Type[];
+	weight: number;
 }
